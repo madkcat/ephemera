9c8c6c21d31e122d46ba4275d26cd6fcc48a9494
madkcat <30823405+madkcat@users.noreply.github.com>
2018-03-06 19:32:47 -0700

    Passport StageOne

--- server.js
+++ server.js
@@ -4,8 +4,13 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const env = require('dotenv').load();
 
 // Sets up the Express App
 // =============================================================
@@ -14,6 +19,7 @@ const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 const db = require("./models");
+const models = require('./models');
 
 // Sets up the Express app to handle data parsing
 
@@ -27,7 +33,19 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
+const authRoute = require('./controllers/auth.js')(app, passport);
 
+// Strategies
+// =============================================================
+require('./config/passport/passport.js')(passport, models.user);
+
+// Passport
+// =============================================================
+app.use(
+  session({ secret: 'rHUyjs6RmVOD06OdOTsVAyUUCxVXaWci', resave: true, saveUninitialized: true })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
@@ -36,3 +54,18 @@ db.sequelize.sync({ force: true }).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+// Sync Database
+models.sequelize
+  .sync()
+  .then(function() {
+    console.log('Database Connected');
+
+    app.listen(8080, function(err) {
+      if (!err) console.log('Connected at http://localhost:8080');
+      else console.log(err);
+    });
+  })
+  .catch(function(err) {
+    console.log(err, 'Error on Database Sync. Please try again!');
+  });
