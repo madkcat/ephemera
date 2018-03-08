ec758eba9a1a07278102d6e7325d80f26925bce2
Patrick Willmore <patman1127@gmail.com>
2018-03-08 01:52:14 -0700

    server & routing works

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,39 +6,33 @@
 // =============================================================
 
 // Requiring our Todo model
-var db = require("../models/questions.js");
-const express = require("express");
-
-const app = express();
+var db = require("../models");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all of the todos
-  app.get("/api/questions", function(req, res) {
+  // GET route for getting all of the qs
+  app.get("/api/hiscores", function(req, res) {
     // findAll returns all entries for a table when used with no options
-    db.Questions.findAll({}).then(function(qs) {
+    db.hiScore.findAll({}).then(function(hiScore) {
       // We have access to the todos as an argument inside of the callback function
-      res.send(qs);
+      res.send(hiScore);
     //   console.log(qs);
     });
     console.log("route works");
   });
 
 
-  // POST route for saving a new todo
-  // app.post("/api/", function(req, res) {
+  // app.get("/api/", function(req, res) {
   //   console.log(req.body);
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
-    // db.qs.create({
-    //   text: req.body.text,
-    //   complete: req.body.complete
-    // }).then(function(qs) {
-      // We have access to the new todo as an argument inside of the callback function
+    // db.qs.findAll({}).then(function(qs) {
+      // We have access to the todos as an argument inside of the callback function
   //     res.send(qs);
+  //     console.log(qs);
   //   });
   // });
 
